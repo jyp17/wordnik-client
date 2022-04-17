15a15527c9d5957c93c52bb236a466fce43f25c4
jyp17 <jypark1024@gmail.com>
2022-04-17 16:29:37 -0700

    Add more queries

--- src/edu/mills/cs180a/wordnik_client_test/Main.java
+++ src/edu/mills/cs180a/wordnik_client_test/Main.java
@@ -1,11 +1,11 @@
 package edu.mills.cs180a.wordnik_client_test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import edu.mills.cs180a.wordnik.client.api.WordsApi;
-import edu.mills.cs180a.wordnik.client.invoker.ApiClient;
-import edu.mills.cs180a.wordnik.client.model.WordOfTheDay;
+import java.io.*;
+import java.nio.charset.*;
+import java.util.*;
+import edu.mills.cs180a.wordnik.client.api.*;
+import edu.mills.cs180a.wordnik.client.invoker.*;
+import edu.mills.cs180a.wordnik.client.model.*;
 
 public class Main {
     private static String getApiKey() throws IOException {
@@ -13,8 +13,7 @@ public class Main {
     }
 
     private static String getResource(String filename) throws IOException {
-        try (InputStream is =
-                Main.class.getResourceAsStream(filename)) {
+        try (InputStream is = Main.class.getResourceAsStream(filename)) {
             if (is == null) {
                 throw new IOException("Unable to open file " + filename);
             }
@@ -27,5 +26,27 @@ public class Main {
         WordsApi wordsApi = client.buildClient(WordsApi.class);
         WordOfTheDay word = wordsApi.getWordOfTheDay("2022-03-15");
         System.out.println(word);
+
+        // get random word
+        String randomWord = wordsApi
+                .getRandomWord("true", "noun", "given-name", 100, -1, 50, -1, 5, 10).getWord();
+
+        // get etymology of random word
+        WordApi wordApi = client.buildClient(WordApi.class);
+        List<String> etymology = wordApi.getEtymologies(randomWord, "false");
+        System.out.println("Etymology of '" + randomWord + "' is: " + etymology);
+
+        // get synonyms of random word
+        List<Related> related = wordApi.getRelatedWords(randomWord, "false", "synonym", 5);
+        List<String> synonyms = related.get(0).getWords();
+        System.out.println("Synonyms of '" + randomWord + "' are: " + synonyms);
+
+        // get frequency of synonyms that are single words only (no phrases)
+        System.out.println("Scrabble score: ");
+        for (String s : synonyms) {
+            if (!s.contains(" ")) {
+                System.out.println(s + ": " + wordApi.getWordFrequency(s, "false", 2020, 2021));
+            }
+        }
     }
 }
